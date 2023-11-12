e2d85a24c0941075f695fcf8083e0f80b9cd214a
SomeBody <maxim.karaichev@saltpepper.ru>
2023-11-12 22:40:17 +0300

    12

--- CurioSpace/asgi.py
+++ CurioSpace/asgi.py
@@ -1,7 +1,14 @@
 import os
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+from chat.routing import ws_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CurioSpace.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns)),
+})
+
